5ea47f351799a18b72a41f7c0e2cd644b171f5ff
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-26 18:48:13 -0600

    create database not table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 -- Delete employee_tracker_db databse if any already exist
 DROP DATABASE IF EXISTS employee_tracker_db;
 
--- Create the database employee_tracker_db
+-- Create the database employee_tracker_db.
 CREATE DATABASE employee_tracker_db;
+
