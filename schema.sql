277fa8146685fcb3810762001a347c1c0a2d08b1
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-26 18:49:55 -0600

    create employee table

--- schema.sql
+++ schema.sql
@@ -8,4 +8,16 @@ CREATE DATABASE employee_tracker_db;
 -- Call which database to use
 USE employee_tracker_db;
 
+-- Create the table employee.
+CREATE TABLE employee (
+  id int NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+
+
+
 
