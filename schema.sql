34e1b748c5538a24dd54a8aee9252694ce250b3f
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-27 14:41:47 -0600

    Adding role table

--- schema.sql
+++ schema.sql
@@ -11,13 +11,19 @@ USE employee_tracker_db;
 -- Create the table employee.
 CREATE TABLE employee (
   id int NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT,
   PRIMARY KEY (id)
 );
 
-
+-- Create the table role.
+CREATE TABLE role (
+  department int NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  PRIMARY KEY (department)
+);
 
 
