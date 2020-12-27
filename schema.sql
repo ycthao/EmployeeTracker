6d9b4061139cbdcac5399e85ca2fdf6386036f04
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-26 18:49:06 -0600

    call which database to use

--- schema.sql
+++ schema.sql
@@ -4,3 +4,8 @@ DROP DATABASE IF EXISTS employee_tracker_db;
 -- Create the database employee_tracker_db.
 CREATE DATABASE employee_tracker_db;
 
+
+-- Call which database to use
+USE employee_tracker_db;
+
+
