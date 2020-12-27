eadb01d9e830a269e0afa08c9f3337c096e6c53c
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-27 15:00:00 -0600

    Adding connection and error troubleshout if cannot connect to database

--- server.js
+++ server.js
@@ -6,14 +6,21 @@ const cTable = require("console.table");
 // connection to mysql database
 const connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "",
     database: "employee_tracker_db"
-  });
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
