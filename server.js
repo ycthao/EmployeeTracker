a71e63517339b7424ccabbdb76e2151f4a866322
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-27 14:58:16 -0600

    creating connection to database

--- server.js
+++ server.js
@@ -2,3 +2,18 @@
 const inquirer = require('inquirer');
 const mysql = require("mysql");
 const cTable = require("console.table");
+
+// connection to mysql database
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "employee_tracker_db"
+  });
