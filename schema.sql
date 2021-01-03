c108ef3cfc3fb0dc0354e0ce1d91724939e992b4
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-03 16:15:55 -0600

    Adding seed to schema

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 -- Create the table employee.
-CREATE TABLE employee (
+CREATE TABLE employee_tbl (
   id int NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -30,7 +30,35 @@ CREATE TABLE role_tbl (
 -- Create the table department_tbl.
 CREATE TABLE department_tbl (
   department_id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (department_id)
 );
 
+
+-- Insert a set of records into employee_tbl
+INSERT INTO employee_tbl (first_name, last_name) 
+	VALUES ('Laurie', 'Wiston');
+INSERT INTO employee_tbl (employee) 
+	VALUES ('Austin', 'Freeman');
+INSERT INTO employee_tbl (employee) 
+	VALUES ('Evelynn', 'Jungle');
+INSERT INTO employee_tbl (employee) 
+	VALUES ('Tiffany', 'Atwater');
+
+-- Insert a set of records into role_tbl
+INSERT INTO role_tbl (title, salary) 
+	VALUES ('Saleman', 50000);
+INSERT INTO role_tbl (roles) 
+	VALUES ('Financial Advisor', 80000);
+   INSERT INTO role_tbl (roles) 
+	VALUES ('IT Support', 65000); 
+INSERT INTO role_tbl (roles) 
+	VALUES ('Manager', 120000);
+    
+    -- Insert a set of records into department_tbl
+INSERT INTO department_tbl (department_name) 
+	VALUES ('Finance');
+INSERT INTO role_tbl (roles) 
+	VALUES ('Sale');
+INSERT INTO role_tbl (roles) 
+	VALUES ('IT');
